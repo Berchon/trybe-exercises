83b555c38ee22ba489c6912890107bea944a9b8d
Berchon <berchon@ggmail.com>
2020-08-27 17:53:18 -0300

    Exercício3

---
+++ modulo1/bloco4/dia1/ex3.js
@@ -0,0 +1,14 @@
+const a = 10;
+const b = 20;
+const c = 15;
+
+//nesse código não me preocupei se algum deles são iuais
+if ((a > b) && (a > c)){
+    console.log(a);
+}
+else if ((b > a) && (b > c)){
+    console.log(b);
+}
+else{
+    console.log(c);
+}
