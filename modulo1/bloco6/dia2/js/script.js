3c9bca5d036b66b7e0bf1fea0aebed7c83712a38
berchon <berchon@gmail.com>
2020-09-22 19:28:10 -0300

    Exercícios do dia 6.3

--- modulo1/bloco6/dia2/js/script.js
+++ modulo1/bloco6/dia2/js/script.js
@@ -1,3 +1,21 @@
-let date = document.getElementById('date').DatePickerX.init();
-
-date.window.DatePickerX.setDefaults('dd/mm/aa')
+let date = document.getElementById('date').DatePickerX.init({
+		format: 'dd/mm/yyyy',
+		mondayFirst: false,
+		weekDayLabels: ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom'],
+		singleMonthLabels: [
+			'Janeiro',
+			'Fevereiro',
+			'Março',
+			'Abril',
+			'Maio',
+			'Junho',
+			'Julho',
+			'Agosto',
+			'Setembro',
+			'Outubro',
+			'Novembro',
+			'Dezembro',
+		],
+		todayButtonLabel: 'Hoje',
+		clearButtonLabel: 'Limpar'
+});
