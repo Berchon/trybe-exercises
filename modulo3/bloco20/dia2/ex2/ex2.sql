cfb211c16afc03e21c0216f32537730206d89589
berchon <berchon@gmail.com>
2021-02-11 20:16:28 -0300

    exercícios do dia 20.2

---
+++ modulo3/bloco20/dia2/ex2/ex2.sql
@@ -0,0 +1,15 @@
+SELECT 'This is SQL Exercise, Practice and Solution';
+SELECT 1, 2, 3;
+SELECT 10 + 15;
+SELECT (10 + 5) * 3;
+SELECT * FROM Scientists.Scientists;
+SELECT name AS 'Nome do Projeto', Hours AS 'Tempo de Trabalho' FROM Scientists.Projects;
+SELECT Name FROM Scientists.Scientists ORDER BY Name;
+SELECT Name FROM Scientists.Projects ORDER BY Name DESC;
+SELECT CONCAT('O projeto ', Name, ' precisou de ', Hours, ' horas para ser concluído.') AS Resumo FROM Scientists.Projects;
+SELECT Name, Hours FROM Scientists.Projects ORDER BY Hours DESC LIMIT 3;
+SELECT DISTINCT Scientist FROM Scientists.AssignedTo ORDER BY Scientist;
+SELECT Name FROM Scientists.Projects ORDER BY Hours DESC LIMIT 1;
+SELECT Name FROM Scientists.Projects ORDER BY Hours DESC LIMIT 1 OFFSET 1;
+SELECT * FROM Scientists.Projects ORDER BY Hours ASC LIMIT 5;
+SELECT CONCAT('Existem ', COUNT(*), ' cientistas na tabela Scientists.') FROM Scientists.Scientists;
