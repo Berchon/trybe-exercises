fa1a60207c9bf170e3e3c552658b0332fc26d47b
Berchon <berchon@gmail.com>
2021-04-13 03:55:59 -0300

    Exercícios do dia 27.1

---
+++ modulo3/bloco27/dia1/examples/model-example-mongodb/models/Book.js
@@ -0,0 +1,52 @@
+// Chamamos a conexão com o banco para poder executar nossa query.
+const connection = require('./connection');
+const { ObjectId } = require('mongodb');
+const Author = require('./Author');
+
+
+// Função que retorna todos os livros cadastrados
+const getAll = () => connection()
+  // Realizamos o `find` de forma muito parecida com o que fazemos no mongo shell,
+  // mas, ao invés de `db.books.find`, fazemos `db.collection('books').find`.
+  .then((db) => db.collection('books').find({}).toArray())
+
+const getByAuthorId = (authorId) => connection()
+  // Novamente, temos um `find` simples, dessa vez utilizando um filtro por ID do autor
+  .then((db) => db.collection('books').find({ author_id: Number(authorId) }).toArray())
+
+const getById = async (id) => {
+  const book = await connection()
+    // Aqui é importante notar que a rota já deve ter verificado que `id` é uma string hexadecimal
+    // de 24 caracteres. Caso contrário, receberemos um erro ao converter o id para `ObjectId`.
+    .then((db) => db.collection('books').findOne(new ObjectId(id)));
+  
+    // Caso o livro não seja encontrado, retornamos `null`.
+    if (!book) return null;
+  
+    // Caso o livro seja encontrado, retornamos seus dados.
+    return book;
+  }
+
+// Método que será chamado quando precisarmos verificar se um livro é válido
+const isValid = async (title, authorId) => {
+  if (!title || typeof title !== 'string' || title.length < 3) return false;
+  // Aqui a única alteração é que `authorId` deve ser uma string de 24 caracteres, e não mais um número
+  if (!authorId || typeof authorId !== 'string' || authorId.length !== 24 || !(await Author.findById(authorId))) return false;
+
+  return true;
+}
+
+// Método que cria um novo livro no banco
+const create = (title, authorId) => connection()
+  // Utilizamos o `insertOne` normalmente para inserir um registro no banco
+  .then((db) => db.collection('books').insertOne({ title, authorId }))
+
+
+// Por último, exportamos a função `getAll` para que ela possa ser usada em outros lugares
+module.exports = {
+  getAll,
+  getByAuthorId,
+  getById,
+  isValid,
+  create,
+}
---
+++ modulo3/bloco27/dia1/readme.md
@@ -0,0 +1,29 @@
+# Dia 1 - Arquitetura de Software - Camada de Model
+
+
+## Descrição
+<p align="justify">
+Dividir sua aplicação em camadas facilita muito a manutenção, a adição de novas funcionalidades e a organização geral do seu código, pois você sabe exatamente onde cada coisa deve ficar.
+
+Quanto mais padrões você conhecer e quanto mais entender em quais cenários cada uma se aplica melhor, maiores as chances de o seu projeto ter sucesso.
+
+Nesse bloco vamos entender um pouco da arquitetura MSC e sobre a camada de modelo.
+</p>
+
+## Sumário
+- [Dia 1 - Arquitetura de Software - Camada de Model
+](#dia-1---arquitetura-de-software---camada-de-dodel)
+- [Descrição](#descrição)
+- [Sumário](#sumário)
+- [Tecnologias utilizadas nesse dia](#tecnologias-utilizadas-nesse-dia)
+- [Conteúdos abordados nesse dia](#conteúdos-abordados-nesse-dia)
+
+## Tecnologias utilizadas nesse dia
+- NodeJS
+- MySQL
+- MongoDB
+
+## Conteúdos abordados nesse dia
+1. Model
+2. Model com MySQL
+3. Model com MongoDB
