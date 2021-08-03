b9d9f0406bbd349efa2bda7983da1c245c95f384
Berchon <berchon@gmail.com>
2021-08-02 22:01:50 -0300

    Dia 37.1 - exercício 3

---
+++ modulo4/bloco37/dia1/exercises/ex3.py
@@ -0,0 +1,13 @@
+def good_pairs(numbers):
+    answer = 0
+    i = 0
+    size = len(numbers)
+    for i in range(size):
+        for j in range(i + 1, size):
+            if numbers[i] == numbers[j]:
+                answer += 1
+    return answer
+
+
+products = [1, 3, 1, 1, 2, 3]
+print(good_pairs(products))
