1ce11737b6e47ea0397b5c8c24554e6683313e91
Berchon <berchon@gmail.com>
2021-08-23 17:45:30 -0300

    Exercisios do dia 38.3

---
+++ modulo4/bloco38/dia3/exercises/ex2.py
@@ -0,0 +1,28 @@
+from stack import Stack
+
+
+class StackOverflow(Exception):
+    pass
+
+
+class LimitStack(Stack):
+    def __init__(self, limit):
+        super().__init__()
+        self.limit = limit
+
+    def push(self, value):
+        if self.size() + 1 > self.limit:
+            raise StackOverflow()
+        super().push(value)
+
+
+stack = LimitStack(2)
+stack.push(1)
+print("Insert number 1")
+stack.push(-2)
+print("Insert number -2")
+
+try:
+    stack.push(5)
+except StackOverflow:
+    print("The Stack is full")
